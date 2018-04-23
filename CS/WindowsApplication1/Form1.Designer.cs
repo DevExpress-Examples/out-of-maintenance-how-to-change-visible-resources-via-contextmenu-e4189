6d074f52f82f986b3c0c3f2be2172a72abf76ab7
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WindowsApplication1/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WindowsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            DevExpress.XtraScheduler.TimeRuler timeRuler1 = new DevExpress.XtraScheduler.TimeRuler();
+            DevExpress.XtraScheduler.TimeRuler timeRuler2 = new DevExpress.XtraScheduler.TimeRuler();
+            this.schedulerControl1 = new DevExpress.XtraScheduler.SchedulerControl();
+            this.schedulerStorage1 = new DevExpress.XtraScheduler.SchedulerStorage(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerStorage1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // schedulerControl1
+            // 
+            this.schedulerControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.schedulerControl1.GroupType = DevExpress.XtraScheduler.SchedulerGroupType.Resource;
+            this.schedulerControl1.Location = new System.Drawing.Point(0, 0);
+            this.schedulerControl1.Name = "schedulerControl1";
+            this.schedulerControl1.Size = new System.Drawing.Size(664, 396);
+            this.schedulerControl1.Start = new System.DateTime(2012, 6, 8, 0, 0, 0, 0);
+            this.schedulerControl1.Storage = this.schedulerStorage1;
+            this.schedulerControl1.TabIndex = 0;
+            this.schedulerControl1.Text = "schedulerControl1";
+            this.schedulerControl1.Views.DayView.ResourcesPerPage = 2;
+            this.schedulerControl1.Views.DayView.TimeRulers.Add(timeRuler1);
+            this.schedulerControl1.Views.GanttView.ResourcesPerPage = 2;
+            this.schedulerControl1.Views.MonthView.ResourcesPerPage = 2;
+            this.schedulerControl1.Views.TimelineView.ResourcesPerPage = 2;
+            this.schedulerControl1.Views.WeekView.ResourcesPerPage = 2;
+            this.schedulerControl1.Views.WorkWeekView.ResourcesPerPage = 2;
+            this.schedulerControl1.Views.WorkWeekView.TimeRulers.Add(timeRuler2);
+            this.schedulerControl1.Click += new System.EventHandler(this.schedulerControl1_Click);
+            this.schedulerControl1.PopupMenuShowing += new DevExpress.XtraScheduler.PopupMenuShowingEventHandler(this.schedulerControl1_PopupMenuShowing);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(474, 301);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 396);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.schedulerControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerStorage1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraScheduler.SchedulerControl schedulerControl1;
+        private DevExpress.XtraScheduler.SchedulerStorage schedulerStorage1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ VB/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,92 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Namespace WindowsApplication1
+	Partial Public Class Form1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Windows Form Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.components = New System.ComponentModel.Container()
+			Dim timeRuler1 As New DevExpress.XtraScheduler.TimeRuler()
+			Dim timeRuler2 As New DevExpress.XtraScheduler.TimeRuler()
+			Me.schedulerControl1 = New DevExpress.XtraScheduler.SchedulerControl()
+			Me.schedulerStorage1 = New DevExpress.XtraScheduler.SchedulerStorage(Me.components)
+			Me.button1 = New System.Windows.Forms.Button()
+			CType(Me.schedulerControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.schedulerStorage1, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.SuspendLayout()
+			' 
+			' schedulerControl1
+			' 
+			Me.schedulerControl1.Dock = System.Windows.Forms.DockStyle.Fill
+			Me.schedulerControl1.GroupType = DevExpress.XtraScheduler.SchedulerGroupType.Resource
+			Me.schedulerControl1.Location = New System.Drawing.Point(0, 0)
+			Me.schedulerControl1.Name = "schedulerControl1"
+			Me.schedulerControl1.Size = New System.Drawing.Size(664, 396)
+			Me.schedulerControl1.Start = New System.DateTime(2012, 6, 8, 0, 0, 0, 0)
+			Me.schedulerControl1.Storage = Me.schedulerStorage1
+			Me.schedulerControl1.TabIndex = 0
+			Me.schedulerControl1.Text = "schedulerControl1"
+			Me.schedulerControl1.Views.DayView.ResourcesPerPage = 2
+			Me.schedulerControl1.Views.DayView.TimeRulers.Add(timeRuler1)
+			Me.schedulerControl1.Views.GanttView.ResourcesPerPage = 2
+			Me.schedulerControl1.Views.MonthView.ResourcesPerPage = 2
+			Me.schedulerControl1.Views.TimelineView.ResourcesPerPage = 2
+			Me.schedulerControl1.Views.WeekView.ResourcesPerPage = 2
+			Me.schedulerControl1.Views.WorkWeekView.ResourcesPerPage = 2
+			Me.schedulerControl1.Views.WorkWeekView.TimeRulers.Add(timeRuler2)
+'			Me.schedulerControl1.Click += New System.EventHandler(Me.schedulerControl1_Click);
+'			Me.schedulerControl1.PopupMenuShowing += New DevExpress.XtraScheduler.PopupMenuShowingEventHandler(Me.schedulerControl1_PopupMenuShowing);
+			' 
+			' button1
+			' 
+			Me.button1.Location = New System.Drawing.Point(474, 301)
+			Me.button1.Name = "button1"
+			Me.button1.Size = New System.Drawing.Size(75, 23)
+			Me.button1.TabIndex = 1
+			Me.button1.Text = "button1"
+			Me.button1.UseVisualStyleBackColor = True
+'			Me.button1.Click += New System.EventHandler(Me.button1_Click);
+			' 
+			' Form1
+			' 
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.ClientSize = New System.Drawing.Size(664, 396)
+			Me.Controls.Add(Me.button1)
+			Me.Controls.Add(Me.schedulerControl1)
+			Me.Name = "Form1"
+			Me.Text = "Form1"
+'			Me.Load += New System.EventHandler(Me.Form1_Load);
+			CType(Me.schedulerControl1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.schedulerStorage1, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.ResumeLayout(False)
+
+		End Sub
+
+		#End Region
+
+		Private WithEvents schedulerControl1 As DevExpress.XtraScheduler.SchedulerControl
+		Private schedulerStorage1 As DevExpress.XtraScheduler.SchedulerStorage
+		Private WithEvents button1 As System.Windows.Forms.Button
+	End Class
+End Namespace
